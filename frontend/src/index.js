ede78cd4a6e4e9885e64e0d7e9fc950609e16e27
Quentin <quentin.lavigne@etu.minesparis.psl.eu>
2026-06-30 14:40:35 +0200

    map css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Map from './Test';
+import Map from './Map';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
