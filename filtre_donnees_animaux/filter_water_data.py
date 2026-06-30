932450f2cd8c873dcead2bf0088452ae939c1694
Anna BS <anna.bergougnoux--santini@etu.minesparis.psl.eu>
2026-06-30 15:08:05 +0200

    tables_triees_water_analysis

---
+++ filtre_donnees_animaux/filter_water_data.py
@@ -0,0 +1,28 @@
+# Install into the SAME Python you run (most reliable)
+
+import pandas as pd
+
+
+Res_ammonium = pd.read_csv('Naiades_filter\Analyses_Ammonium.CSV', sep=";", on_bad_lines="skip", dtype=str)
+print(Res_ammonium)
+
+Res_temp = pd.read_csv('Naiades_filter\Analyses_Temp.CSV', sep=";", on_bad_lines="skip", dtype=str)
+Res_DBO = pd.read_csv('Naiades_filter\Analyses_DBO5.CSV', sep=";", on_bad_lines="skip", dtype=str)
+print (Res_temp)
+print(Res_DBO) 
+
+
+print(Res_ammonium[['CdStationMesureEauxSurface','LbStationMesureEauxSurface','CdSupport','LbSupport','CdFractionAnalysee','LbFractionAnalysee','CdPrelevement','DatePrel','HeurePrel','DateAna','HeureAna','CdParametre','LbLongParamètre','RsAna','CdUniteMesure','SymUniteMesure'
+]])
+
+ammonium_tri=Res_ammonium[['CdStationMesureEauxSurface','LbStationMesureEauxSurface','CdSupport','LbSupport','CdFractionAnalysee','LbFractionAnalysee','CdPrelevement','DatePrel','HeurePrel','DateAna','HeureAna','CdParametre','LbLongParamètre','RsAna','CdUniteMesure','SymUniteMesure'
+]]
+
+temp_tri=Res_temp[['CdStationMesureEauxSurface','LbStationMesureEauxSurface','CdSupport','LbSupport','CdFractionAnalysee','LbFractionAnalysee','CdPrelevement','DatePrel','HeurePrel','DateAna','HeureAna','CdParametre','LbLongParamètre','RsAna','CdUniteMesure','SymUniteMesure'
+]]
+
+dbo_tri=Res_DBO[['CdStationMesureEauxSurface','LbStationMesureEauxSurface','CdSupport','LbSupport','CdFractionAnalysee','LbFractionAnalysee','CdPrelevement','DatePrel','HeurePrel','DateAna','HeureAna','CdParametre','LbLongParamètre','RsAna','CdUniteMesure','SymUniteMesure'
+]]
+
+
+
